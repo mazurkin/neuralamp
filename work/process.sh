98f702e069ffe4f2be9e59cf3baed44d3931940e
Nick Mazurkin <mazurkin@gmail.com>
2026-05-10 14:02:46 -0400

    update

--- work/process.sh
+++ work/process.sh
@@ -18,29 +18,45 @@ declare -r -x PACKAGE_DIR
 readarray -d '' WAV_FILES < <(find "${SCRIPT_DIR}/captures" -maxdepth 1 -type f -name "*.wav" -print0)
 declare -r WAV_FILES
 
+# length of the input file in samples
+INPUT_FILE_LEN=$(soxi -s "${SCRIPT_DIR}/input.wav")
+declare -r INPUT_FILE_LEN
+
+# input file statistics
+echo "*** Input: input.wav"
+sox "${SCRIPT_DIR}/input.wav" -n stats
+
 # conversion
 function build_model() {
     local -r WAV_FILE=${1?No path to the WAV file}
 
-    # file name
-    local -r WAV_FILE_NAME=$(basename "${WAV_FILE}")
+    # statistics
+    echo "*** Capture: ${WAV_FILE}"
+    sox "${WAV_FILE}" -n stats
 
     # calculate the length of the wav file in samples
     local -r WAV_FILE_LEN=$(soxi -s "${WAV_FILE}")
-    echo "${WAV_FILE_NAME} / ${WAV_FILE_LEN}"
 
-    # trim if we need to trim
-    if (( WAV_FILE_LEN > 9120000 ))
-    then
-        sox "${WAV_FILE}" "${SCRIPT_DIR}/output.wav" trim "0s" "9120000s"
-    fi
+    # remove old output WAV file (if exists)
+    rm --force "${SCRIPT_DIR}/output.wav"
 
-    # pad the end if the need to pad
-    if (( WAV_FILE_LEN < 9120000 ))
+    # process the capture WAV file
+    if (( WAV_FILE_LEN > INPUT_FILE_LEN ))
     then
-        local -r WAV_FILE_PAD=$(echo "9120000 - ${WAV_FILE_LEN}" | bc)
+        # trim if we the capture WAV file is longer than the input file
+        sox "${WAV_FILE}" "${SCRIPT_DIR}/output.wav" trim "0s" "${INPUT_FILE_LEN}s"
+    elif (( WAV_FILE_LEN < INPUT_FILE_LEN ))
+    then
+        # pad the end if the capture WAV file is shorter than the input file
+        local -r WAV_FILE_PAD=$(echo "${INPUT_FILE_LEN} - ${WAV_FILE_LEN}" | bc)
         sox "${WAV_FILE}" "${SCRIPT_DIR}/output.wav" pad "0s" "${WAV_FILE_PAD}s"
+    else
+        # just copy the file without processing
+        cp "${WAV_FILE}" "${SCRIPT_DIR}/output.wav"
     fi
+
+    # file name
+    local -r WAV_FILE_NAME=$(basename "${WAV_FILE}")
 
     # create the model's folder
     local -r MODEL_FOLDER="${SCRIPT_DIR}/models/${WAV_FILE_NAME%.wav}"
